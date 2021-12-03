85c0b7eb97f0a5f5fb8129e5cfb73451cbc6e3a5
matrjx <jakub.prucnal@gmail.com>
2021-12-03 23:25:05 +0100

    Gulls with numbers

--- Lab4/SVM_gulls.py
+++ Lab4/SVM_gulls.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing, svm
 from sklearn.decomposition import PCA
 from sklearn.impute import KNNImputer
-# import sklearn.metrics as sm
+import sklearn.metrics as sm
 
 """
 Opracowanie:
@@ -98,3 +98,10 @@ plt.ylim(X_pca[:, 1].min() - 0.1, X_pca[:, 1].max() + 0.1)
 plt.legend(*scatter.legend_elements())
 
 plt.show()
+
+print("Linear regressor performance:")
+print("Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred), 2))
+print("Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred), 2)) 
+print("Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred), 2)) 
+print("Explain variance score =", round(sm.explained_variance_score(y_test, y_test_pred), 2))
+print("R2 score =", round(sm.r2_score(y_test, y_test_pred), 2))
