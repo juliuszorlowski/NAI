bd7ee5ad5c2e88e39acdb69472bd611c2c8a531f
Juliusz Orłowski <orlowski.juliusz@gmail.com>
2021-12-03 17:56:42 +0100

    Added documentation

--- Lab4/SVM_pima_diabetes.py
+++ Lab4/SVM_pima_diabetes.py
@@ -8,6 +8,40 @@ from sklearn.decomposition import PCA
 from sklearn.impute import KNNImputer
 # import sklearn.metrics as sm
 
+"""
+Opracowanie:
+    Autorzy: Jakub Prucnal
+             Juliusz Orłowski
+    Temat:   Uczenie SVM klasyfikowania danych
+Wejście:
+    - plik pima-indians-diabetes.csv zawierający zbiór danych z 768 badań medycznych przeprowadzonych na Indianach Pima
+    pod kątem przewidywania wystąpienia cukrzycy w ciągu 5 lat od badania
+    Dane zawierają:
+        1. Ilość ciąż
+        2. Poziom glukozy we krwi
+        3. Rozkurczowe ciśnienie krwi (mm Hg)
+        4. Grubość skóry nad tricepsem (mm)
+        5. Ilość insuliny w surowicy (mu U/ml)
+        6. Indeks BMI (waga w kg/(wzrost w m)^2)
+        7. Funkcja cukrzycy genetycznej
+        8. Wiek (w latach)
+        9. Zmienna klasowa (0 lub 1)
+Wyjście:
+    Program wykorzystuje Support Vector Classificator w celu klasyfikacji danych do dwóch zbiorów:
+        1. Nie zagrożony wystąpieniem cukrzycy
+        2. Zagrożony wystąpieniem cukrzycy
+Wykorzystywane biblioteki:
+    NumPy - do tworzenia macierzy
+    matplotlib - do analizy danych
+    pandas - do tworzenia wizualizacji danych
+
+Dokumentacja kodu źródłowego:
+    Python -> docstring (https://www.python.org/dev/peps/pep-0257/)
+    NumPy -> https://numpy.org/doc/stable/user/whatisnumpy.html
+    matplotlib -> https://matplotlib.org/
+    pandas -> https://pandas.pydata.org/
+"""
+
 def dataset(data):
     data = pd.DataFrame(data)
 
