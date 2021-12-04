e67a6d74195f162bd05dd302c8c54d526aff08a7
matrjx <jakub.prucnal@gmail.com>
2021-12-04 15:28:20 +0100

    Lab5 start

---
+++ Lab5/neural_network_pima.py
@@ -0,0 +1,93 @@
+from math import sqrt
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing, svm
+from sklearn.decomposition import PCA
+from sklearn.impute import KNNImputer
+
+"""
+Opracowanie:
+    Autorzy: Jakub Prucnal
+             Juliusz Orłowski
+    Temat:   Uczenie Neural Network klasyfikowania danych
+Wejście:
+    - plik pima-indians-diabetes.csv zawierający zbiór danych z 768 badań medycznych przeprowadzonych na Indianach Pima
+    pod kątem przewidywania wystąpienia cukrzycy w ciągu 5 lat od badania
+    Dane zawierają:
+        1. Ilość ciąż
+        2. Poziom glukozy we krwi
+        3. Rozkurczowe ciśnienie krwi (mm Hg)
+        4. Grubość skóry nad tricepsem (mm)
+        5. Ilość insuliny w surowicy (mu U/ml)
+        6. Indeks BMI (waga w kg/(wzrost w m)^2)
+        7. Funkcja cukrzycy genetycznej
+        8. Wiek (w latach)
+        9. Zmienna klasowa (0 lub 1)
+Wyjście:
+    Program wykorzystuje ????? w celu klasyfikacji danych do dwóch zbiorów:
+        1. Nie zagrożony wystąpieniem cukrzycy
+        2. Zagrożony wystąpieniem cukrzycy
+Wykorzystywane biblioteki:
+    NumPy - do tworzenia macierzy
+    matplotlib - do tworzenia wizualizacji danych
+    pandas - do analizy danych
+    scikit-learn - do uczenia i przewidywania sieci neuronowych
+
+Dokumentacja kodu źródłowego:
+    Python -> docstring (https://www.python.org/dev/peps/pep-0257/)
+    NumPy -> https://numpy.org/doc/stable/user/whatisnumpy.html
+    matplotlib -> https://matplotlib.org/
+    pandas -> https://pandas.pydata.org/
+"""
+
+input_file = 'pima-indians-diabetes.csv'
+data = pd.read_csv(input_file)
+
+# Cleaning dataset with kNN-Imputer
+# Replace 0 -> Null
+data[['Glucose','BloodPressure','SkinThickness','Insuline','BMI']] = data[
+    ['Glucose','BloodPressure','SkinThickness','Insuline','BMI']
+    ].replace(0,np.NaN)
+
+X, y = data.loc[:,[
+    'Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insuline', 'BMI', 'DiabetesPedigreeFunction', 'Age'
+    ]], data.loc[:,['Outcome']]
+
+
+# Using k-NN imputer replace NaN -> kNNValue
+knn = KNNImputer()
+knn.fit(X)
+new_X = knn.transform(X)
+new_X = pd.DataFrame(new_X)
+
+
+# Scaling
+new_X = preprocessing.minmax_scale(new_X)
+new_X = pd.DataFrame(new_X)
+
+# PCA transformation - Merge all columns in new_X to 2 colums.
+X_pca = PCA(n_components=2).fit_transform(new_X)
+
+y = y.astype(int).values
+y = y.ravel()
+
+# Train and test split
+num_training = int(0.8 * len(X))
+num_test = len(X) - num_training
+
+# Training data
+X_train, y_train = X_pca[:num_training], y[:num_training]
+
+# Test data
+X_test, y_test = X_pca[num_training:], y[num_training:]
+
+# create a mesh to plot in
+x_min, x_max = X_pca[:, 0].min() - 1, X_pca[:, 0].max() + 1
+y_min, y_max = X_pca[:, 1].min() - 1, X_pca[:, 1].max() + 1
+
+h = sqrt(((x_max / x_min)/100)**2)
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+ np.arange(y_min, y_max, h))
+
