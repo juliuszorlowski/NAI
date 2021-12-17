1abd48218341d0a3e9e81fe100b90aab8c1dbeed
matrjx <jakub.prucnal@gmail.com>
2021-12-17 23:27:41 +0100

    Logi do animals oraz Number four

---
+++ Lab5/Number_4.py
@@ -0,0 +1,101 @@
+import warnings
+import pandas as pd
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.neural_network import MLPClassifier
+
+"""
+Opracowanie:
+    Autorzy: Jakub Prucnal
+             Juliusz Orłowski
+    Temat:   Sieci Neuronowe dla Klasyfikacji
+
+Wejście:
+    W celu uruchomienia programu należy pobrać plik csv ze strony https://www.openml.org/d/180 i przenieść go do
+    katalogu głównego programu pod nazwą covertype.csv.
+
+    - plik covertype.csv zawierający zbiór danych 80 milionów małych obrazów wielkości 32x32 pikseli reprezentujących
+    10 klas obiektów.
+
+    Kolumny: 
+        Name / Data Type / Measurement / Description
+        Elevation / quantitative /meters / Elevation in meters
+        Aspect / quantitative / azimuth / Aspect in degrees azimuth
+        Slope / quantitative / degrees / Slope in degrees
+        Horizontal_Distance_To_Hydrology / quantitative / meters / Horz Dist to nearest surface water features
+        Vertical_Distance_To_Hydrology / quantitative / meters / Vert Dist to nearest surface water features
+        Horizontal_Distance_To_Roadways / quantitative / meters / Horz Dist to nearest roadway
+        Hillshade_9am / quantitative / 0 to 255 index / Hillshade index at 9am, summer solstice
+        Hillshade_Noon / quantitative / 0 to 255 index / Hillshade index at noon, summer solstice
+        Hillshade_3pm / quantitative / 0 to 255 index / Hillshade index at 3pm, summer solstice
+        Horizontal_Distance_To_Fire_Points / quantitative / meters / Horz Dist to nearest wildfire ignition points
+        Wilderness_Area (4 binary columns) / qualitative / 0 (absence) or 1 (presence) / Wilderness area designation
+        Soil_Type (40 binary columns) / qualitative / 0 (absence) or 1 (presence) / Soil Type designation
+        Cover_Type (7 types) / integer / 1 to 7 / Forest Cover Type designation
+
+    Etykiety danych:
+        Etykieta 	Opis
+        0 	        airplane
+        1 	        automobile
+        2 	        bird
+        3 	        cat
+        4 	        deer
+        5       	dog
+        6 	        frog
+        7       	horse
+        8       	ship
+        9 	        truck
+
+Wyjście:
+    Program wyświetla log z kolejnymi iteracjami uczenia oraz ostatecznym wynikiem treningowym i testowym.
+
+Wykorzystywane biblioteki:
+    pandas - do analizy danych
+    matplotlib - do tworzenia wizualizacji danych
+
+Dokumentacja kodu źródłowego:
+    Python -> docstring (https://www.python.org/dev/peps/pep-0257/)
+    matplotlib -> https://matplotlib.org/
+    pandas -> https://pandas.pydata.org/
+"""
+
+input_file = 'covertype.csv'
+data = pd.read_csv(input_file, header=None, skiprows=[0])
+
+X, y = data.loc[0:, :3071], data.loc[0:, 3072:]
+X = X / 255.0
+
+# Return a flattened array
+y = y.astype(int).values
+y = y.ravel()
+
+# Train and test split
+num_training = int(0.8 * len(X))
+num_test = len(X) - num_training
+
+# Training data
+X_train, y_train = X[:num_training], y[:num_training]
+
+# Test data
+X_test, y_test = X[num_training:], y[num_training:]
+
+# Neural Network Classifier - 3 hidden layers with sizes: 1000, 400 and 84
+mlp = MLPClassifier(
+    hidden_layer_sizes=(1000, 400, 84,),
+    max_iter=20,
+    alpha=1e-4,
+    solver="sgd",
+    verbose=10,
+    random_state=1,
+    learning_rate_init=0.1,
+)
+
+# Catching warnings from MLPClassifier
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=ConvergenceWarning, module="sklearn")
+
+    # Here is a kind of Magic of Machine Learning
+    mlp.fit(X_train, y_train)
+
+# Printing score for train and test dataset.
+print("Training set score: %f" % mlp.score(X_train, y_train))
+print("Test set score: %f" % mlp.score(X_test, y_test))
