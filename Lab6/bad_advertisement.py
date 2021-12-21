b472071a34683027f39ea18f7bc876683de3691c
Jakub Prucnal <jakub.prucnal@gmail.com>
2021-12-21 20:53:00 +0100

    zmiany

--- Lab6/bad_advertisement.py
+++ Lab6/bad_advertisement.py
@@ -1,4 +1,7 @@
 import cv2
+import skvideo.io
+import mediapipe as mp
+
 
 def cascade(path):
     result = cv2.CascadeClassifier(path)
@@ -8,14 +11,6 @@ def cascade(path):
 
     return result
 
-# def face_cascade():
-#     result = cv2.CascadeClassifier(
-#     'data/haarcascades/haarcascade_frontalface_default.xml')
-
-#     if result.empty():
-#         raise IOError('Unable to load the cascade classifier xml file')
-
-#     return result
 
 def video_capture(number):
     result = cv2.VideoCapture(number)
@@ -26,6 +21,7 @@ def video_capture(number):
     
     return result
 
+
 def rects(cascade, frame, no1, no2, no3, blue, green, red):
     result = cascade.detectMultiScale(frame, no1, no2)
 
@@ -35,20 +31,33 @@ def rects(cascade, frame, no1, no2, no3, blue, green, red):
 
 
 cap0 = video_capture(0)
+cap_ad = video_capture('./adv.mp4')
 
 while True:
 
-    _, frame0 = cap0.read()
+    _, frame_0 = cap0.read()
+    _, frame_ad = cap_ad.read()
 
     face_cascade = cascade('data/haarcascades/haarcascade_frontalface_default.xml')
+    face_rects = rects(face_cascade, frame_0, 1.3, 7, 2, 0, 255, 0)
+
     eye_cascade = cascade('data/haarcascades/haarcascade_eye.xml')
+    eye_rects = rects(eye_cascade, frame_0, 1.3, 5, 3, 0, 255, 0)
 
-    face_rects = rects(face_cascade, frame0, 1.3, 5, 3, 0, 255, 0)
-    eye_rects = rects(eye_cascade, frame0, 1.3, 5, 3, 0, 255, 0)
+    # glasses_cascade = cascade('data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+    # glasses_rects = rects(glasses_cascade, frame_0, 1.3, 5, 1, 0, 255, 255)
 
-    cv2.imshow('Face and Eye Detector', frame0)
+    # righteye_cascade = cascade('data/haarcascades/haarcascade_righteye_2splits.xml')
+    # righteye_rects = rects(righteye_cascade, frame_0, 1.3, 7, 2, 255, 0, 0)
+
+    # lefteye_cascade = cascade('data/haarcascades/haarcascade_lefteye_2splits.xml')
+    # lefteye_rects = rects(lefteye_cascade, frame_0, 1.3, 7, 2, 0, 0, 255)
+
+    cv2.imshow('Face and Eye Detector', frame_0)
+    # cv2.imshow('Advertisement', frame_ad)
     if cv2.waitKey(1) == 27:
         break
+
 
 cap0.release()
 cv2.destroyAllWindows()
